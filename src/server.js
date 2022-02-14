37755d2871e7fb76846a7f10e5aa0bfda8055047
Minna <minna1025@gmail.com>
2022-02-14 21:44:29 +0900

    #1 CHAT WITH WEBSOCKETS - nicknames part two

--- src/server.js
+++ src/server.js
@@ -19,10 +19,23 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected to the Browser ❌")); // 브러우저를 껐을때
   socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+    const messageString = message.toString("utf8");
+    const messageObj = JSON.parse(messageString);
+
+    switch (messageObj.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${messageObj.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = messageObj.payload;
+        break;
+    }
   });
 });
 
