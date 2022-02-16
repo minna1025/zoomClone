1bfd755d836694eb5c0fa5ae9d903e1f860a84c9
Minna <minna1025@gmail.com>
2022-02-16 23:53:34 +0900

    #2 SOCKETIO - user count

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -53,14 +53,20 @@ function handleRoomSubmit(event) {
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (user) => addMessage(`${user} joined!`));
+socket.on("welcome", (user, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerHTML = `Room ${roomName} (${newCount})`;
+  addMessage(`${user} joined!`);
+});
 
-socket.on("bye", (left) => addMessage(`${left} left ㅠㅠ`));
+socket.on("bye", (left, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerHTML = `Room ${roomName} (${newCount})`;
+  addMessage(`${left} left ㅠㅠ`);
+});
 
 socket.on("new_message", (msg) => addMessage(msg));
 
-// socket.on("room_change", console.log);
-// socket.on("room_change", (msg) => console.log(msg)));
 socket.on("room_change", (rooms) => {
   const roomList = welcome.querySelector("ul");
   roomList.innerHTML = "";
--- src/server.js
+++ src/server.js
@@ -32,6 +32,10 @@ function publickRooms() {
   return publickRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
@@ -41,13 +45,13 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publickRooms());
   });
 
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname)
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
     );
   });
 
