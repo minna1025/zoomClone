be90b1302b4c62dcf253156e84cf1afbb28c4d3d
Minna <minna1025@gmail.com>
2022-02-16 22:49:11 +0900

    #2 SOCKETIO - room notifications

--- src/server.js
+++ src/server.js
@@ -20,10 +20,20 @@ wsServer.on("connection", (socket) => {
   socket.onAny((event) => {
     console.log(`Socket event: ${event}`);
   });
+
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome");
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
   });
 });
 
