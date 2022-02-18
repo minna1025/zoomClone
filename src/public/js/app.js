d12f0fa998bd8109349e140851563d7b27b119f8
Minna <minna1025@gmail.com>
2022-02-18 21:22:30 +0900

    #3 VIDEO CALL - stun

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -154,7 +154,19 @@ socket.on("ice", (ice) => {
 
 //RTC Code
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun.l.google.com:19302",
+          "stun1.l.google.com:19302",
+          "stun2.l.google.com:19302",
+          "stun3.l.google.com:19302",
+          "stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
