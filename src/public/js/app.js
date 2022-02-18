11968ee941be71ac016e3c17e8002d87c4b6a525
Minna <minna1025@gmail.com>
2022-02-18 17:35:48 +0900

    #3 VIDEO CALL - rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,14 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
   try {
@@ -51,8 +55,6 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
-
 function handleMuteBtn() {
   myStream
     .getAudioTracks()
@@ -88,3 +90,28 @@ function handleCameraChange() {
 }
 
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function stratMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, stratMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket code
+socket.on("welcome", () => {
+  console.log("somenone joined");
+});
