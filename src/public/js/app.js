570e8349569f6d09a435487ccd5e51de02156b5b
Minna <minna1025@gmail.com>
2022-02-18 16:58:16 +0900

    #3 VIDEO CALL - camera switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (e) {
@@ -24,14 +28,24 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  // deviceId가 없을때
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -68,3 +82,9 @@ function handleCameraBtn() {
 }
 
 cameraBtn.addEventListener("click", handleCameraBtn);
+
+function handleCameraChange() {
+  getMedia(camerasSelect.value);
+}
+
+camerasSelect.addEventListener("input", handleCameraChange);
